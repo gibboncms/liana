ad8f7eafc152b06e9e3f14cc9ff22acd5271e8e1
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-06-14 12:23:16 +0200

    Bootstrap fix; Env defaults change

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -7,6 +7,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 /* Load environment variables
 ---------------------------------------------------------*/
-if (file_exists(__DIR__.'/../.env'))
+if (file_exists(__DIR__.'/../.env')) {
     Dotenv::load(__DIR__.'/../');
 }
